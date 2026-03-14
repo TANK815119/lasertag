a6980fe8af00eeba401e927671a6bb5dfbb91859
Reid Jacobsen <52799675+TANK815119@users.noreply.github.com>
2026-03-14 15:03:54 -0500

    Drone explosions can now kill player

--- Assets/com.rekabsen.autodrone/Scripts/Dronetrix/Explode.cs
+++ Assets/com.rekabsen.autodrone/Scripts/Dronetrix/Explode.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Anaglyph.Lasertag.Logistics;
+using Anaglyph.Lasertag;
 
 namespace Rekabsen.AutoDrone
 {
@@ -8,9 +10,12 @@ namespace Rekabsen.AutoDrone
     {
         [SerializeField] private float explosionRadius = 5f;
         [SerializeField] private float explosionForce = 700f;
+		[SerializeField] private float maxDamage = 200f;
 
-        // Start is called before the first frame update
-        void Start()
+		private Bullet.DamageData damageData;
+
+		// Start is called before the first frame update
+		void Start()
         {
             TriggerExplosion(transform.position, explosionRadius, explosionForce);
         }
@@ -18,14 +23,30 @@ namespace Rekabsen.AutoDrone
         void TriggerExplosion(Vector3 position, float radius, float force)
         {
             Collider[] colliders = Physics.OverlapSphere(position, radius);
-            foreach (Collider hit in colliders)
+            foreach (Collider col in colliders)
             {
-                Rigidbody rb = hit.GetComponent<Rigidbody>();
+				// Attempt to damage
+				damageData = new Bullet.DamageData
+				{
+					playerID = 0, // Zero for now, should be NetworkPlayerID of the drone's owner
+					damage = calculateDamage(this.transform, col.transform)
+				};
+
+				col.transform.root.BroadcastMessage("OnShot", damageData, SendMessageOptions.DontRequireReceiver);
+
+				// Add force
+				Rigidbody rb = col.GetComponent<Rigidbody>();
                 if (rb != null)
                 {
                     rb.AddExplosionForce(force, position, radius, 1f, ForceMode.Impulse);
                 }
             }
         }
-    }
+
+		private float calculateDamage(Transform explosionOrigin, Transform victim)
+		{
+			float distance = Vector3.Distance(explosionOrigin.position, victim.position);
+			return maxDamage / distance * distance; // Simple inverse square falloff
+		}
+	}
 }
